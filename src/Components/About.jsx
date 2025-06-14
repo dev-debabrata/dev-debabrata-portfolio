a6d46130397ad5f192c34ec100f8f3fe75be121f
dev-debabrata <debabratadas711@gmail.com>
2025-06-15 03:18:52 +0530

    15-06-2025

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import AboutImg from "../assets/about.png";
-import { MdMailOutline } from 'react-icons/md';
-import { FiMapPin } from 'react-icons/fi';
+import SkillsImg from "../assets/skills.png";
+import EducationsImg from "../assets/educations.png";
 import { infoData } from '../data';
+
 
 const tabs = [
     { id: "tab1", label: "Personal Info" },
@@ -11,23 +12,6 @@ const tabs = [
 
 
 
-const qualificationData = [
-    {
-        title: "Education",
-        data: [
-            {
-                school: "Nupat Technology, Lagos",
-                qualification: "Software Development",
-                years: "2022 - 2023",
-            },
-            {
-                school: "Lagos State University, Lagos",
-                qualification: "BSC",
-                years: "2017 - 2022",
-            },
-        ],
-    },
-];
 
 const skillsData = [
     {
@@ -89,116 +73,74 @@ const About = () => {
                     <h1 className=" text-5xl font-semibold">About me</h1>
                 </div>
                 {/* About section */}
-                <div className="mt-10 gap-20 flex flex-col-reverse md:flex-row justify-between items-center">
-                    {/* Left side */}
+                <div className="flex justify-between items-center gap-10">
                     <div>
-                        <img
-                            src={AboutImg} alt="Debabrata Das" className=" max-w-[200px] object-cover md:w-[500px] md:h-[200px]" />
+                        <img src={AboutImg} alt="About" className=" max-w-[350px]" />
                     </div>
-                    {/* Right side */}
-                    <div className="flex-1">
-                        <div defaultValue="Personal">
-                            <div className="w-full grid xl:grid-cols-3 xl:max-w-[520px] xl:border dark:border-none">
-                                <button className="w-[162px] xl:w-auto" value="Personal">
-                                    Personal Info
-                                </button>
-                                <button
-                                    className="w-[162px] xl:w-auto"
-                                    value="Qualifications"
-                                >
-                                    Qualifications
-                                </button>
-                                <button className="w-[162px] xl:w-auto" value="Skills">
-                                    Skills
-                                </button>
-                            </div>
-                            {/* Tab Content */}
-                            <div className="text-lg mt-12 xl:mt-8">
-                                {/* Personal Tab */}
-                                <div value="Personal">
-                                    <div className="text-center xl:text-left">
-                                        <p className="subtitle max-w-x mx-auto xl:mx-0">
-                                            Hey there! I’m a web developer who’s all about blending
-                                            creativity with innovative technology to create engaging
-                                            online experiences. With a toolkit that includes
-                                            JavaScript, TypeScript, Next.js, and TailwindCSS, I’ve
-                                            helped companies create intuitive interfaces, seamless
-                                            user experiences, and high-quality, responsive
-                                            applications. <br />
-                                            But let’s be real {""}
-                                            <span className="text-[#e6f0ff] font-medium">
-                                                what truly sets me apart
-                                            </span>{" "}
-                                            is my passion for excellence and user-focused design. For
-                                            me, it’s not just about writing clean code, it’s about
-                                            delivering a product that doesn’t just work but wows.
-                                        </p>
-                                        <p className="subtitle max-w-xl mx-auto xl:mx-0">
-                                            When I’m not coding, you’ll find me devouring books,
-                                            exploring the latest in tech, or binge-watching a new
-                                            series. Curious about my work? Let’s connect and create
-                                            something awesome together!
-                                        </p>
-                                        {/* Icons */}
-                                        <div className="grid xl:grid-cols-2 gap-4 mb-12">
-                                            {infoData.map((item, index) => {
-                                                return (
-                                                    <div
-                                                        className="flex items-center gap-x-4 mx-auto xl:mx-0"
-                                                        key={index}
-                                                    >
-                                                        <div className="text-primary "><MdMailOutline size={20} /></div>
-                                                        <div className="text-primary "><FiMapPin size={20} /></div>
-                                                        <div>{item.text}</div>
-                                                    </div>
-                                                );
-                                            })}
-                                        </div>
+                    <div className="">
+                        <p className=" text-lg">
+                            Hello! I'm Debabrata Das, a B-Tech student in Computer Science and Engineering with a strong passion for technology, programming, and problem-solving. I enjoy exploring various areas of computer science including web development, database management, and algorithms. I'm currently building a solid foundation in both theoretical and practical aspects of computing.
+                        </p>
+                        {/* Icons */}
+                        <div className=" grid grid-rows-2 gap-2 mt-4">
+                            {infoData.map((item, index) => {
+                                const Icon = item.icon;
+                                return (
+                                    <div
+                                        className="flex items-center gap-x-4 mx-auto xl:mx-0"
+                                        key={index}
+                                    >
+                                        <div className="text-primary "><Icon size={20} /></div>
+                                        <div>{item.text}</div>
                                     </div>
-                                </div>
-
-                                {/* Qualifications Tab */}
-                                <div value="Qualifications">
-                                    <div className="">
-                                        <h3 className="h3 mb-8 text-center xl:text-left">
-                                            My Journey
-                                        </h3>
-                                        {/* Experience and Education */}
-
-                                    </div>
-                                </div>
-
-                                {/* Skills Tab */}
-                                <div value="Skills">
-                                    <div className="text-center xl:text-left">
-                                        {/* <h4 className="h3 mb-8">Tools I use Daily</h4> */}
-                                        <h4 className="h3 mb-8">Experience Tools</h4>
-                                        <div className="mb-16">
-                                            {/* Skills */}
-                                            <h4 className="text-xl font-semibold mb-2">
-                                                Skills/Technologies
-                                            </h4>
-                                            <div className="border-b border-border mb-4"></div>
-
-                                            {/* Skill List */}
-
-                                        </div>
-
-                                        {/* Tools */}
-                                        <div>
-                                            <h4 className="text-4xl font-semibold mb-2 xl:text-left">
-                                                Tools
-                                            </h4>
-                                            <div className="border-b border-border mb-4"></div>
-                                            {/* Tools List */}
-
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                                );
+                            })}
                         </div>
                     </div>
                 </div>
+
+
+                {/*Educations and Skills section */}
+                <div className="mt-5">
+                    <div>
+                        <div className=" flex items-center justify-between gap-20 ">
+                            <button className="text-[24px] font-semibold transition duration-300 transform hover:scale-105 rounded-full inline-flex justify-center items-center bg-amber-600 py-3 px-8">
+                                Skills
+                            </button>
+                            <button>
+                                Educations
+                            </button>
+                        </div>
+                        <div>
+                            <div>
+                                <img src={SkillsImg} alt="" className="max-w-[300px]" />
+                            </div>
+                            <div>
+                                <p>
+
+                                </p>
+                            </div>
+                            <div>
+                                <img src={EducationsImg} alt="" className="max-w-[300px]" />
+                            </div>
+                            <div>
+                                <p>
+
+                                </p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+
+
+
+
+
+
+
+
                 <div className=" flex w-full justify-around mt-20">
                     <div className="flex flex-col items-center gap-2.5 transition">
                         <h1 className="text-[60px] font-bold bg-clip-text">0+</h1>
@@ -221,3 +163,12 @@ const About = () => {
 }
 
 export default About
+
+
+
+
+
+
+
+
+
--- src/data.js
+++ src/data.js
@@ -1,3 +1,5 @@
+import { FiMapPin } from "react-icons/fi";
+import { MdMailOutline } from "react-icons/md";
 
 
 
@@ -33,8 +35,50 @@ export const NavbarMenu = [
 
 
 // About section data
+export const EXAMPLES = {
+    education: {
+        title: 'Education',
+        description:
+            'Components are the building blocks of React applications. A component is a self-contained module (HTML + optional CSS + JS) that renders some output.',
+        code: `
+  function Welcome() {
+    return <h1>Hello, World!</h1>;
+  }`,
+    },
+    skills: {
+        title: 'Skills',
+        description:
+            'JSX is a syntax extension to JavaScript. It is similar to a template language, but it has full power of JavaScript (e.g., it may output dynamic content).',
+
+    },
+};
+
 export const infoData = [
 
-    { id: 1, text: "debabratadas711@gmail.com" },
-    { id: 2, text: "Kolkata, India" },
+    {
+        icon: MdMailOutline,
+        text: "debabratadas711@gmail.com"
+    },
+    {
+        icon: FiMapPin,
+        text: "Kolkata, West Bengal, India"
+    },
+];
+
+export const qualificationData = [
+    {
+        school: "Dr. Sudhir Chandra Sur Institute of Technology & Sports Complex",
+        qualification: "Computer Science and Engineering",
+        years: "2023 - PRESENT",
+    },
+    {
+        school: "Kingston Educational Institute",
+        qualification: "Mechanical Engineering",
+        years: "2016 - 2019",
+    },
+    {
+        school: "Purunda Ramkrishna Siksha Sadhan",
+        qualification: "H.S",
+        years: "2014 - 2016",
+    },
 ];
