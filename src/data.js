6e2aadc3d7339f275ee6f6f7d453054a6e1fb6f8
dev-debabrata <debabratadas711@gmail.com>
2025-06-24 02:59:29 +0530

    24-06-2025

--- src/data.js
+++ src/data.js
@@ -443,9 +443,6 @@ export const projectData = [
         webapp: "https://tictactoe-dev.netlify.app/",
     },
 
-
-    ////////////////////////////
-    // Mern stack
     {
         id: 2,
         image: ElearningImg,
@@ -457,10 +454,6 @@ export const projectData = [
         webapp: "https://education-learnig-dev.netlify.app/",
     },
 
-
-
-    /////////////////////////////////////
-    // JavaScript
     {
         id: 3,
         image: BankTransactionImg,
@@ -491,93 +484,4 @@ export const projectData = [
         github: "https://github.com/dev-debabrata/projects-javascript/tree/main/modal-show",
         webapp: "https://dev-debabrata.github.io/projects-javascript/modal-show",
     },
-    // {
-    //     id: 0,
-    //     title: "Coca Cola",
-    //     description:
-    //         "A powerful and user-friendly React.js application designed to uncover and showcase detailed GitHub profile information.",
-    //     image: CocaColaImg,
-    //     tags: ["HTML", "CSS", "JavaScript", "React JS", "API"],
-    //     github: "",
-    //     webapp: "",
-    // },
-    // {
-    //     id: 1,
-    //     title: "E-Learning",
-    //     description:
-    //         "A full-stack quiz-based platform designed for GATE/UGC NET students to practice previous year questions and create customized tests.",
-    //     image: ElearningImg,
-    //     tags: ["React JS", "Node.js", "MongoDB", "Express", "HTML", "CSS", "JavaScript"],
-    //     github: "",
-    // },
-    // {
-    //     id: 2,
-    //     title: "Tic Tac Toe",
-    //     description:
-    //         "A React-based web application that provides movie recommendations based on different criteria.",
-    //     image: TicTacToeImg,
-    //     tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
-    //     github: "",
-    //     webapp: "",
-    // },
-    // {
-    //     id: 3,
-    //     title: "Email ",
-    //     description:
-    //         "An efficient and customizable NPM package for validating email addresses. Built using React.js.",
-    //     image: TicTacToeImg,
-    //     tags: ["React JS", "Node.js", "NPM", "Validation"],
-    //     github: "",
-    //     webapp: "",
-    // },
-    // {
-    //     id: 4,
-    //     title: "Task Reminder Chrome Extension Tool",
-    //     description:
-    //         "A productivity-boosting Chrome extension designed to help users manage and remember their daily tasks.",
-    //     image: TicTacToeImg,
-    //     tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
-    //     github: "",
-    //     webapp: "",
-    // },
-    // {
-    //     id: 5,
-    //     title: "Webverse Digital",
-    //     description:
-    //         "The official website for Webverse Digital, a creative digital marketing agency. Built using HTML, CSS, and JavaScript, it features visually appealing animations and a clean design to showcase the agency's services.",
-    //     image: webverLogo,
-    //     tags: ["HTML", "CSS", "JavaScript", "Framer Motion"],
-    //     github: "https://github.com/codingmastr/Webverse-Digital",
-    //     webapp: "https://webversedigital.com/",
-    // },
-    // {
-    //     id: 6,
-    //     title: "Coding Master",
-    //     description:
-    //         "An ed-tech platform where users can access tech and coding-related blogs, notes, interview questions, e-books, and premium content with payment integration. Built with full-stack technologies for a seamless learning experience.",
-    //     image: cmLogo,
-    //     tags: ["React JS", "Node.js", "MongoDB", "Express", "Payment Integration"],
-    //     github: "https://codingmasterweb.in/",
-    //     webapp: "https://codingmasterweb.in/",
-    // },
-    // {
-    //     id: 7,
-    //     title: "Image Search App",
-    //     description:
-    //         "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
-    //     image: imagesearchLogo,
-    //     tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
-    //     github: "https://github.com/codingmastr/Image-Search-App",
-    //     webapp: "https://imagsearch.netlify.app/",
-    // },
-    // {
-    //     id: 8,
-    //     title: "Image Background Remover",
-    //     description:
-    //         "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
-    //     image: removebgLogo,
-    //     tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
-    //     github: "https://github.com/codingmastr/Image-Background-Remover",
-    //     webapp: "https://removeyourbg.netlify.app/",
-    // },
 ];  
