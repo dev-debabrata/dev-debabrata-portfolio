7099d797149799db04c9cde70f17b68ad1e0f24c
dev-debabrata <debabratadas711@gmail.com>
2025-07-15 10:33:55 +0530

    modified

--- src/Components/ThemeToggler.jsx
+++ src/Components/ThemeToggler.jsx
@@ -9,15 +9,11 @@ const ThemeToggler = () => {
     return (
         <button
             onClick={toggleTheme}
-            className="relative w-10 h-10 flex items-center justify-center rounded-full border dark:border-gray-500 border-blue-400 hover:bg-gray-950 dark:hover:bg-blue-500 transition"
+            className="relative w-10 h-10 flex items-center justify-center rounded-full border dark:border-gray-500 border-blue-400 hover:bg-gray-950 dark:hover:bg-blue-700 transition"
             aria-label="Toggle Theme"
         >
-            <IoSunnyOutline
-                className={`h-6 w-6 transition-all ${themeMode === 'dark' ? 'rotate-90 scale-0' : 'rotate-0 scale-100'}`}
-            />
-            <BsMoonStars
-                className={`absolute h-6 w-6 transition-all ${themeMode === 'dark' ? 'rotate-0 scale-100' : 'rotate-90 scale-0'}`}
-            />
+            <IoSunnyOutline className={`h-6 w-6 transition-all ${themeMode === 'dark' ? 'rotate-90 scale-0' : 'rotate-0 scale-100'}`} />
+            <BsMoonStars className={`absolute h-6 w-6 transition-all ${themeMode === 'dark' ? 'rotate-0 scale-100' : 'rotate-90 scale-0'}`} />
         </button>
     );
 };
