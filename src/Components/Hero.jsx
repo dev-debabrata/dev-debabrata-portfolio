e4be96f0d5fdffb03bda1843223918b25d381468
dev-debabrata <debabratadas711@gmail.com>
2025-07-17 16:22:10 +0530

    modified

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
     return (
         <section
             id="home"
-            className="dark:bg-slate-900 py-12 pt-25 px-[7vw] md:px-[7vw] lg:px-[10vw] ">
+            className="dark:bg-slate-900 py-12 pt-30 px-[7vw] md:px-[7vw] lg:px-[10vw] ">
             <div className="flex xl:flex flex-col-reverse xl:gap-12 md:gap-10 gap-5 md:flex-row justify-between items-center my-2">
                 {/* Left side */}
                 <div className="text-center md:text-left mt-8 md:mt-0">
--- src/Components/ThemeToggler.jsx
+++ src/Components/ThemeToggler.jsx
@@ -9,7 +9,7 @@ const ThemeToggler = () => {
     return (
         <button
             onClick={toggleTheme}
-            className="relative w-9 h-9 md:w-10 md:h-10 lg:w-10 lg:h-10 xl:w-10 xl:h-10 flex items-center justify-center rounded-full border dark:border-gray-500 border-blue-400 hover:text-white hover:bg-blue-600 dark:hover:bg-blue-600 transition"
+            className="relative w-9 h-9 md:w-10 md:h-10 lg:w-10 lg:h-10 xl:w-10 xl:h-10 flex items-center justify-center rounded-full border dark:border-gray-400 border-blue-400 hover:text-white hover:bg-blue-800 dark:hover:bg-white dark:hover:text-black transition"
             aria-label="Toggle Theme"
         >
             <IoSunnyOutline className={`h-5 w-5 md:w-6 md:h-6 lg:w-6 lg:h-6 xl:w-6 xl:h-6 transition-all ${themeMode === 'dark' ? 'rotate-90 scale-0' : 'rotate-0 scale-100'}`} />
