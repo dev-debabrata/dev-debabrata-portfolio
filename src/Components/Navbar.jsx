a84595ddbe170854813b0b67b8df843ed72da2dd
dev-debabrata <debabratadas711@gmail.com>
2025-08-19 11:30:22 +0530

    19-08-2025

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -169,7 +169,7 @@ const Navbar = () => {
                         <SiLeetcode size={35} />
                     </a> */}
                 </div>
-                <div className="hidden md:flex lg:flex xl:flex cursor-pointer">
+                <div className="hidden md:flex lg:flex xl:flex">
                     <ThemeToggler />
                 </div>
 
